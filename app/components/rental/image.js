6965ec94ed8fe24f7743c4916328dc5bd0d9326d
Daya Shankar <dayashankar2001@gmail.com>
2024-03-12 00:31:10 +0530

    First Commit - Basics

---
+++ app/components/rental/image.js
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class RentalImageComponent extends Component {
+//   isLarge = false;
+  @tracked isLarge = false;
+
+  @action toggleSize() {
+    this.isLarge = !this.isLarge;
+    // console.log("Clicked");
+  }
+}
